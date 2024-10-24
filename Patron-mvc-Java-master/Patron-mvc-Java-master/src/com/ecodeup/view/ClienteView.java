0fe3f8ded39cb508b7bdbd65546ab8126d421619
Leonardo Fernandes <147807012+leosife@users.noreply.github.com>
2024-10-24 17:34:06 +0200

    111

--- Patron-mvc-Java-master/Patron-mvc-Java-master/src/com/ecodeup/view/ClienteView.java
+++ Patron-mvc-Java-master/Patron-mvc-Java-master/src/com/ecodeup/view/ClienteView.java
@@ -2,7 +2,7 @@ package com.ecodeup.view;
 
 public class ClienteView {
 	public void imprimirDatosCliente(int id,String nombre, String apellido) {
-		System.out.println("**** DATOS MIOSS ****");
+		System.out.println("**** DATOS MIOSSSS ****");
 		System.out.println("Id: "+id);
 		System.out.println("Nombre: "+nombre);
 		System.out.println("Apellido: "+apellido);
